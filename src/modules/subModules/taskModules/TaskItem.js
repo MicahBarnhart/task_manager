6bae7496b11b3a8a5aefc23d850db71b55b839b4
Micah Barnhart <micah.barnhart815@gmail.com>
2020-01-19 09:45:50 -0800

    got the timer to stop and start and maintain the correct time, also changed the layout of the task

--- src/modules/subModules/taskModules/TaskItem.js
+++ src/modules/subModules/taskModules/TaskItem.js
@@ -9,22 +9,20 @@ class TaskItem extends React.Component{
             totalTime: "0:00",
             startTime: null,
             interval: null,
+            timerToggle: "Not Running"
         };
-        this.StartTimerClick = this.StartTimerClick.bind(this);
-        this.StopTimerClick = this.StopTimerClick.bind(this);
+        this.TimerButtonClick = this.TimerButtonClick.bind(this);
         this.LogTimeDifference = this.LogTimeDifference.bind(this);
     }
     render(){
         return(
             <div >
                 <label className="taskAdderSpacing">{this.state.itemName}</label>
-                <button className="taskAdderSpacing"
-                    onClick={this.StartTimerClick}>
+                <button id="timerButton" className="taskAdderSpacing"
+                    onClick={this.TimerButtonClick}>
                     Start Timer
                 </button>
-                <button className="taskAdderSpacing" onClick={this.StopTimerClick}>
-                    Stop Timer
-                </button>
+                <label className="taskAdderSpacing">Timer is: {this.state.timerToggle}</label>
                 <label className="taskAdderSpacing">{this.state.totalTime}</label>
             </div>
         );
@@ -32,21 +30,32 @@ class TaskItem extends React.Component{
     onComponentDidMount(){
 
     }
-    StartTimerClick(){
-        if (this.state.startTime == null)
-            this.setState({startTime: new Date()})
-        else
-            this.setState({startTime: ((new Date().getMilliseconds()/1000) + this.state.timeElapsed)});
-        console.log("Total time when started: " + this.state.startTime);
-        //console.log("start time:" + this.state.startTime);
-        this.setState({interval: setInterval(this.LogTimeDifference, 1000)});
-    }
-    StopTimerClick(){
-        let now = new Date();
-        let total = (now - this.state.startTime) / 1000;
-        console.log("Total time when stopped: " + total);
-        this.setState({timeElapsed: total});
-        clearInterval(this.state.interval);
+    TimerButtonClick(){
+        if (this.state.timerToggle === "Not Running"){
+            if (this.state.startTime == null){
+                let now = new Date();
+                this.setState({startTime: now})
+                //console.log("Start time: " + now / 1000);
+            }
+            else{
+                let now = new Date().setMilliseconds(-this.state.timeElapsed * 1000);
+                this.setState({startTime: now});
+                //console.log("Start time now: " + now / 1000);
+            }
+            //console.log("Total time when started: " + this.state.startTime);
+            //console.log("start time:" + this.state.startTime);
+            this.setState({interval: setInterval(this.LogTimeDifference, 1000), timerToggle: "Running"});
+            //document.getElementById("timerButton").innerHTML = "Stop Timer";
+        }
+        else{
+            let now = new Date();
+            let total = Math.floor((now - this.state.startTime) / 1000);
+            //console.log("Total time when stopped: " + total);
+            this.setState({timeElapsed: total, timerToggle: "Not Running"});
+            clearInterval(this.state.interval);
+            document.getElementById("timerButton").innerHTML = "Start Timer";
+        }
+        
     }
     LogTimeDifference(){
         let now = new Date() - this.state.startTime;
@@ -55,7 +64,7 @@ class TaskItem extends React.Component{
         let hours = Math.floor(now / 3600);
         let remainder = Math.floor(now % 3600);
         let minutes = Math.floor(remainder / 60);
-        let seconds = Math.floor(remainder % 60);        
+        let seconds = Math.floor(remainder % 60);
 
         let timeSpent = "Hours: " + hours + " Minutes: " + minutes + " Seconds: " + seconds;
         this.setState({totalTime: timeSpent});
