c5b2f34b8542a3c52f781ebe11eb02fe3824b3a5
Micah Barnhart <micah.barnhart815@gmail.com>
2020-01-19 21:02:04 -0800

    Updates

---
+++ precache-manifest.b2208424f1c11214ea8983b77fcd801a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0880b60b45570265050d4a0a9dfd2adb",
+    "url": "/task_manager/index.html"
+  },
+  {
+    "revision": "81affabbf01371839016",
+    "url": "/task_manager/static/css/main.1c409819.chunk.css"
+  },
+  {
+    "revision": "b1207c0fb57799c5a3bd",
+    "url": "/task_manager/static/js/2.45c68da5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/task_manager/static/js/2.45c68da5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "81affabbf01371839016",
+    "url": "/task_manager/static/js/main.ef7a2d6a.chunk.js"
+  },
+  {
+    "revision": "edb2b325d854971b4d4e",
+    "url": "/task_manager/static/js/runtime-main.364062f2.js"
+  }
+]);
